ac1fbfb77fc8548e9cd1a4031dcb1168e0ca08ad
Lee-Shawn <425984393@qq.com>
2016-03-02 11:16:23 +0800

    注释

--- src/day02/Demo01.java
+++ src/day02/Demo01.java
@@ -3,7 +3,7 @@ package day02;
 public class Demo01 {
 	public static void main(String[] args) {
 		int[] arr1 = new int[3];
-		int[] arr2 = arr1;
+		int[] arr2 = arr1;// 数组arr2指向arr1
 		arr1[1] = 11;
 		System.out.println(arr1[1]);
 		arr2[1] = 22;
