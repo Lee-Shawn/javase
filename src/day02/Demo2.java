3b375bdccf00bb753fbf93dad92b0a14eb11ad4d
Lee-Shawn <425984393@qq.com>
2016-03-02 13:38:50 +0800

    数组中常见的问题

---
+++ src/day02/Demo2.java
@@ -0,0 +1,10 @@
+package day02;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		int[] a = new int[3];
+		a = null;
+		//a[1] = 1; // a=null再赋值，出现空指针异常
+		//System.out.println(a[1]);
+	}
+}
