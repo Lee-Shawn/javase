125c0e62bef07a32d106a53e2787851d2ccdced5
Lee-Shawn <425984393@qq.com>
2016-03-16 18:51:26 +0800

    frame

---
+++ src/day15/FrameUtil.java
@@ -0,0 +1,18 @@
+package day15;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class FrameUtil {
+	public static void frame(JFrame frame, int width, int height) {
+		Toolkit t = Toolkit.getDefaultToolkit();
+		Dimension d = t.getScreenSize();
+		int x = d.width;
+		int y = d.height;
+		frame.setBounds((x-width)/2, (y-height)/2, width, height);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
