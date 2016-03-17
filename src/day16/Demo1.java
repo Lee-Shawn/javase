94ad928c8295e90eb6aaf5179cfdae3aa2309ae8
Lee-Shawn <425984393@qq.com>
2016-03-17 14:30:32 +0800

    set

---
+++ src/day16/Demo1.java
@@ -0,0 +1,13 @@
+package day16;
+
+import java.util.HashSet;
+
+public class Demo1 {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+		HashSet hashSet = new HashSet();
+		hashSet.add("111");
+		hashSet.add("222");
+		System.out.println(hashSet);
+	}
+}
