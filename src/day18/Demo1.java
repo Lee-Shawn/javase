0e2bfa8015a747120ebf7125bac19ed7017738b0
Lee-Shawn <425984393@qq.com>
2016-03-19 18:02:51 +0800

    for each

---
+++ src/day18/Demo1.java
@@ -0,0 +1,10 @@
+package day18;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		int[] array = {1, 3, 6, 3, 6, 2};
+		for (int a : array) {
+			System.out.println(a);
+		}
+	}
+}
